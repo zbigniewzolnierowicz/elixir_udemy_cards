e324cf26d2b4147085484884932749758895a640
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-02 23:11:00 +0200

    Format stuff

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,21 @@
 defmodule Cards do
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits, value <- values do
@@ -17,7 +32,7 @@ defmodule Cards do
   end
 
   def deal(deck, amount) do
-    { hand, _rest_of_deck } = Enum.split(deck, amount)
+    {hand, _rest_of_deck} = Enum.split(deck, amount)
     hand
   end
 
@@ -28,14 +43,14 @@ defmodule Cards do
 
   def load(filename) do
     case File.read(filename) do
-      {:ok, binary} -> :erlang.binary_to_term binary
+      {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> "That binary does not exist."
     end
   end
 
   def create_hand(amount) do
-    Cards.create_deck
-      |> Cards.shuffle
-      |> Cards.deal(amount)
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(amount)
   end
 end
