513439c60ac1c11a7c4a7ced359a602325f6c87b
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-02 22:29:56 +0200

    Make the deck creation better

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,11 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(unshuffled_deck) do
