89d2f0a514bcdf081819fa2a4635f9f274a09265
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-02 23:38:41 +0200

    Add unit tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,15 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "Cards.create_deck creates 52 cards" do
+    deck_size = length(Cards.create_deck)
+    assert deck_size == 52
+  end
+
+  test "Cards.shuffle randomizes the deck" do
+    deck = Cards.create_deck
+    shuffled_deck = Cards.shuffle(deck)
+    refute deck == shuffled_deck
+  end
 end
